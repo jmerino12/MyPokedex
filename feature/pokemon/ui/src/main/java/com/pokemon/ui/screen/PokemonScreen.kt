1505fb30205611f88f6de745d4df6731a2a5a5af
Jonathan Eduardo Meriño Bolivar <jmerino12@merqueo.com>
2024-08-09 10:45:26 -0500

    innerpadding

--- feature/pokemon/ui/src/main/java/com/pokemon/ui/screen/PokemonScreen.kt
+++ feature/pokemon/ui/src/main/java/com/pokemon/ui/screen/PokemonScreen.kt
@@ -2,13 +2,20 @@ package com.pokemon.ui.screen
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.WindowInsetsSides
+import androidx.compose.foundation.layout.asPaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.only
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.systemBars
+import androidx.compose.foundation.layout.windowInsetsPadding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -26,33 +33,38 @@ fun PokemonScreen(
     LaunchedEffect(Unit) {
         getPokemons()
     }
-    when {
-        pokemonUiState is PokemonUiState.LOADING -> {
-            Column(
-                modifier = Modifier.fillMaxSize(),
-                verticalArrangement = Arrangement.Center,
-                horizontalAlignment = Alignment.CenterHorizontally
-            ) {
-                CircularProgressIndicator(
+    Scaffold { innerPadding ->
+        when {
+            pokemonUiState is PokemonUiState.LOADING -> {
+                Column(
                     modifier = Modifier
-                        .padding(16.dp)
-                )
-            }
-
-        }
-
-        pokemonUiState is PokemonUiState.SUCCESS -> {
-            LazyColumn(
-                modifier = modifier.fillMaxSize(),
-                verticalArrangement = Arrangement.spacedBy(4.dp),
-            ) {
-                items(pokemonUiState.pokemons) {
-                    PokemonItem(pokemon = it)
+                        .fillMaxSize()
+                        .padding(innerPadding),
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    CircularProgressIndicator(
+                        modifier = Modifier
+                            .padding(16.dp)
+                    )
                 }
+
+            }
+            pokemonUiState is PokemonUiState.SUCCESS -> {
+                LazyColumn(
+                    modifier = modifier
+                        .fillMaxSize()
+                        .padding(innerPadding),
+                    verticalArrangement = Arrangement.spacedBy(4.dp),
+                ) {
+                    items(pokemonUiState.pokemons) {
+                        PokemonItem(pokemon = it)
+                    }
+                }
+
             }
 
         }
-
     }
 
 }
